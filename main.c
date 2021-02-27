59b600f65cab0c7b6681fcad93244537a4f3132c
test <test>
2021-02-27 19:36:15 +0100

    funkcija

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
+int racun(int a, int b)
+{
+	if(a > b)
+		return a - b;
+	else
+		return b - a;
+}
+
 int main()
 {
 	int a = 5;
 	int b = 8;
 	int c;
 	
-	if(a > b)
-		c = a - b;
-	else
-		c = b - a;
+	c = racun(a, b);
 
 	print("Rezultat je: %d\n", c);
 
